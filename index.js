5d5c302e890cb0020c8e6460b3815d9e6edf6adb
mencreat <mencreatkecret@gmail.com>
2025-09-05 03:28:00 +0700

    modified:   index.js

--- index.js
+++ index.js
@@ -1,28 +1,4 @@
-// const app = require('./server')
+const app = require('../server'); // import Express app dari server.js
+const serverless = require('serverless-http');
 
-// const port = process.env.PORT || 5000
-
-// app.listen(port, () => {
-//     console.log(`server runing in http://localhost:${port}`)
-// })
-
-{
-  "version": 2,
-  "builds": [
-    {
-      "src": "app.js",
-      "use": "@vercel/node"
-    }
-  ],
-  "routes": [
-    {
-      "src": "/api/(.*)",
-      "dest": "app.js"
-    },
-    {
-      "src": "/(.*)",
-      "dest": "app.js"
-    }
-  ]
-}
-  
+module.exports = serverless(app);
