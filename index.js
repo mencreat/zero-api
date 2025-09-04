396b8371ba22ec4f164fda0fcd8d860fc46754f4
mencreat <mencreatkecret@gmail.com>
2025-09-05 03:21:51 +0700

    modified:   index.js

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
-const app = require('./server')
+// const app = require('./server')
 
-const port = process.env.PORT || 5000
+// const port = process.env.PORT || 5000
 
-app.listen(port, () => {
-    console.log(`server runing in http://localhost:${port}`)
-})
+// app.listen(port, () => {
+//     console.log(`server runing in http://localhost:${port}`)
+// })
+
+{
+  "version": 2,
+  "builds": [
+    {
+      "src": "app.js",
+      "use": "@vercel/node"
+    }
+  ],
+  "routes": [
+    {
+      "src": "/api/(.*)",
+      "dest": "app.js"
+    },
+    {
+      "src": "/(.*)",
+      "dest": "app.js"
+    }
+  ]
+}
+  
