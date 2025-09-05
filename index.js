980ca44d396a8c0f10acca3da25c925d03717ede
mencreat <mencreatkecret@gmail.com>
2025-09-06 00:43:03 +0700

    testing deploy

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-// const app = require('./server')
+const app = require('./server')
 
-// const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000
 
-// app.listen(port, () => {
-//     console.log(`server runing in http://localhost:${port}`)
-// })
+app.listen(port, () => {
+    console.log(`server runing in http://localhost:${port}`)
+})
 
-{
-  "version": 2,
-  "builds": [
-    {
-      "src": "server.js",
-      "use": "@vercel/node"
-    }
-  ],
-  "routes": [
-    {
-      "src": "/zeronewatch-api/(.*)",
-      "dest": "server.js"
-    },
-    {
-      "src": "/(.*)",
-      "dest": "server.js"
-    }
-  ]
-}
+// {
+//   "version": 2,
+//   "builds": [
+//     {
+//       "src": "server.js",
+//       "use": "@vercel/node"
+//     }
+//   ],
+//   "routes": [
+//     {
+//       "src": "/zeronewatch-api/(.*)",
+//       "dest": "server.js"
+//     },
+//     {
+//       "src": "/(.*)",
+//       "dest": "server.js"
+//     }
+//   ]
+// }
   
